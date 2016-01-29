fa3d9c15c1ba6b0efa6feefb43d7f359520ca78c
Tos Alles <talles@sogyo.nl>
2016-01-29 16:11:03 +0100

    With lists it is much nicer :)

--- src/main/scala/nl/sogyo/fp/lists.scala
+++ src/main/scala/nl/sogyo/fp/lists.scala
@@ -1,11 +1,9 @@
 package nl.sogyo.fp
 
-import scala.collection.mutable.ArrayBuffer
-
 object lists {
-  def filterEvens(list: ArrayBuffer[Int], result: ArrayBuffer[Int]): ArrayBuffer[Int] = {
-    if(list.length == 0)                 result
-    else if(arithmetics.modulus(list.head, 2) == 0) filterEvens(list.tail, result += list.head)
-    else                                 filterEvens(list.tail, result)
+  def filterEvens(list: List[Int], result: List[Int]): List[Int] = {
+    if(list.length == 0) result.reverse
+    else if(arithmetics.modulus(list.head, 2) == 0) filterEvens(list.tail, list.head :: result)
+    else filterEvens(list.tail, result)
   }
 }
--- src/test/scala/nl/sogyo/fp/listTest.scala
+++ src/test/scala/nl/sogyo/fp/listTest.scala
@@ -2,23 +2,21 @@ package nl.sogyo.fp
 
 import org.scalatest.FunSuite
 
-import scala.collection.mutable.ArrayBuffer
-
 class listTest extends FunSuite {
   test("filter evens from [1,2,3,4] == [2,4]") {
-    assert(lists.filterEvens(ArrayBuffer(1,2,3,4), ArrayBuffer()) == ArrayBuffer(2,4))
+    assert(lists.filterEvens(List(1,2,3,4), List()) == List(2,4))
   }
 
   test("filter evens from [] == []") {
-    assert(lists.filterEvens(ArrayBuffer(), ArrayBuffer()) == ArrayBuffer())
+    assert(lists.filterEvens(List(), List()) == List())
   }
 
   test("filter evens from [2,4,6] == [2,4,6]") {
-    assert(lists.filterEvens(ArrayBuffer(2,4,6), ArrayBuffer()) == ArrayBuffer(2,4,6))
+    assert(lists.filterEvens(List(2,4,6), List()) == List(2,4,6))
   }
 
   test("filter evens from [1,3,5] == []") {
-    assert(lists.filterEvens(ArrayBuffer(1,3,5), ArrayBuffer()) == ArrayBuffer())
+    assert(lists.filterEvens(List(1,3,5), List()) == List())
   }
 
 }
