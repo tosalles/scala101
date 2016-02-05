df54aff77b2d8f09234cf1de408fc49c2e650ac8
Tos Alles <talles@sogyo.nl>
2016-02-05 11:45:13 +0100

    Addded exercises with map function

--- src/test/scala/nl/sogyo/fp/higherOrderTest.scala
+++ src/test/scala/nl/sogyo/fp/higherOrderTest.scala
@@ -10,4 +10,14 @@ class higherOrderTest extends FunSuite {
   test("Apply division function to two ints") {
     assert(higherOrder.apply(arithmetics.division, 9, 3) == 3)
   }
+
+  test("Apply division to list of ints with map") {
+    val list = List(2,4,6)
+    assert(list.map(i => i / 2) == List(1,2,3))
+  }
+
+  test("Convert every boolean in the list to its counterpart") {
+    val list = List(true,false,true)
+    assert(list.map(b => !b) == List(false,true,false))
+  }
 }
