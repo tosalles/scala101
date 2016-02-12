44f31abdebadf22c724a8f72d392d143c690ce46
Tos Alles <talles@sogyo.nl>
2016-02-12 12:01:45 +0100

    Added more exercises for higher order functions

--- src/test/scala/nl/sogyo/fp/higherOrderTest.scala
+++ src/test/scala/nl/sogyo/fp/higherOrderTest.scala
@@ -7,6 +7,18 @@ class higherOrderTest extends FunSuite {
     assert(higherOrder.apply(arithmetics.sum, 1, 1) == 2)
   }
 
+  test("Apply a function to a list to test if it contains an even number") {
+    val list = List(1,2,3)
+    def isEven(n: Int) = n % 2 == 0
+    assert(list.exists(isEven))
+  }
+
+  test("Define a function increment (x+1) and map this function to a list of numbers") {
+    val list = List(1,2,3)
+    def increment(x: Int) = x + 1
+    assert(list.map(increment) == List(2,3,4))
+  }
+
   test("Apply division function to two ints") {
     assert(higherOrder.apply(arithmetics.division, 9, 3) == 3)
   }
@@ -14,6 +26,11 @@ class higherOrderTest extends FunSuite {
   test("Apply division to list of ints with map") {
     val list = List(2,4,6)
     assert(list.map(i => i / 2) == List(1,2,3))
+  }
+
+  test("Return all even numbers from a list") {
+    val list = List(1,2,3,4,5,6,7,8,9,10)
+    assert(list.filter(n => n % 2 == 0) == List(2,4,6,8,10))
   }
 
   test("Convert every boolean in the list to its counterpart") {
@@ -28,6 +45,6 @@ class higherOrderTest extends FunSuite {
 
   test("Return a list of booleans based on a list of characters, where false means whitespace, true on characters") {
     val list = "Ik werk bij Sogyo".toCharArray.toList
-    assert(list.map(c => if(c == ' ') false; else true) == List(true, true, false, true, true, true, true, false, true, true, true, false, true, true, true, true, true))
+    assert(list.map(c => if(c == ' ') false else true) == List(true, true, false, true, true, true, true, false, true, true, true, false, true, true, true, true, true))
   }
 }
