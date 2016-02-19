e369ab2274a7640f4cee387945a0ee608ec0d6aa
Kevin van der Vlist <kvdvlist@sogyo.nl>
2016-02-19 13:55:48 +0100

    alternate map/filter func impl

--- src/main/scala/nl/sogyo/fp/HigherOrderFunctions.scala
+++ src/main/scala/nl/sogyo/fp/HigherOrderFunctions.scala
@@ -1,11 +1,12 @@
 package nl.sogyo.fp
 
 object HigherOrderFunctions {
+
   implicit class myList[A](l: List[A]) {
 
-    def ownFilter(f: A => Boolean): List[A] = {
+    def ownFilterImperative(f: A => Boolean): List[A] = {
       var result: List[A] = List[A]()
-      for(element <- this.l) {
+      for (element <- this.l) {
         if (f(element)) {
           result = result :+ element
         }
@@ -13,12 +14,28 @@ object HigherOrderFunctions {
       result
     }
 
-    def ownMap[B](f: A => B): List[B] = {
+    def ownFilter(f: A => Boolean): List[A] = l match {
+      case head :: tail => {
+        if (f(head)) {
+          head :: tail.ownFilter(f)
+        } else {
+          tail.ownFilter(f)
+        }
+      }
+      case Nil => List()
+    }
+
+    def ownMapImperative[B](f: A => B): List[B] = {
       var result: List[B] = List[B]()
-      for(element <- this.l) {
+      for (element <- this.l) {
         result = result :+ f(element)
       }
       result
+    }
+
+    def ownMap[B](f: A => B): List[B] = l match {
+      case head :: tail => f(head) :: tail.ownMap(f)
+      case Nil => List()
     }
   }
 
